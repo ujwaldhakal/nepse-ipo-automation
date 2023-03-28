bafce48bbd8bd87593b84b63f3b837140ca5b286
Ujwal Dhakal <ujwal.dhakal@werkspot.nl>
2023-03-28 21:21:13 +0200

    Initial setup for applying ipo for newly listed companies

---
+++ README.md
@@ -0,0 +1,7 @@
+## Nepse IPO Automation
+This is automation written in cyrpess to apply for IPO whenever there is a new issue for stock market.
+With this you would need to make some changes in `cypress.env.json` file with your credentials and it will apply only ordinary shares at the moment.
+
+### Installation
+- If you are using docker then run `docker-compose up` the video will be available once it gets completed
+- If you dont want to use docker you can run `npm install` && `npm run cypress-headless` it will execute the code to apply the IPO
---
+++ cypress/e2e/apply-ipo.js
@@ -0,0 +1,41 @@
+describe('Login Test', () => {
+    it('Should apply IPO ', () => {
+      // Visit the login page
+    
+      let baseUrl = Cypress.env('app_url')
+
+      cy.visit(baseUrl+'#/login');
+      cy.get('.serarchable-dropdown').click().type(Cypress.env('login_bank_name'));
+      cy.get('.select2-results__option--highlighted').click()
+      cy.get('#username').type(Cypress.env('login_bank_username'));
+      cy.get('#password').type(Cypress.env('login_bank_password'));
+      cy.get('.sign-in').click();
+      cy.wait(1000)
+      cy.window().then( win => {
+        cy.visit(baseUrl+'#/asba');
+        cy.wait(2000)
+        cy.get('.company-list').each(($el, index, $list) => {
+          applyIpo($el) // apply multiple ipo in a loop
+        })
+    })
+    });
+  });
+
+
+  function applyIpo(el) 
+  {
+    let shareType = el.find('.isin').text();
+    if(shareType === 'Ordinary Shares') 
+    {
+      
+        el.find('.btn-issue').click(); // apply
+        cy.wait(1000);
+        cy.get('#selectBank').select(Cypress.env('bank_name_to_apply_ipo')); 
+        cy.get('#appliedKitta').type(Cypress.env('number_of_kitta'))
+        cy.get('#crnNumber').type(Cypress.env('crn'))
+        cy.get('#disclaimer').click()
+        cy.get('.card-footer > .btn-primary').click()
+        cy.get('#transactionPIN').type(Cypress.env('transaction_pin'))
+        cy.get('.confirm-page-btn > .btn-primary').click()
+    }
+  }
